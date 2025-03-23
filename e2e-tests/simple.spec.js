ea53f4ab58897550eddc9c5df05bc22973d244aa
Joe Hsiao <joe.phsiao@gmail.com>
2025-03-23 15:03:18 -0700

    add a playwright test for subpage.

--- e2e-tests/simple.spec.js
+++ e2e-tests/simple.spec.js
@@ -1,9 +1,17 @@
-const { test, describe, expect } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('')
+  })
+  test('front page can be opened', async ({ page }) => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+  test('ivysaur page can be opened', async ({ page }) => {
+    await page.click('text=ivysaur')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('overgrow')).toBeVisible()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
