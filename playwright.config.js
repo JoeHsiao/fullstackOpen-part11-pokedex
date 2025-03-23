0a222a7118b6fd92874d11df30211a806273272d
Joe Hsiao <joe.phsiao@gmail.com>
2025-03-23 12:01:06 -0700

    remove the step which starts up server manually

--- playwright.config.js
+++ playwright.config.js
@@ -73,7 +73,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'npm run start-prod',
     url: 'http://127.0.0.1:5001',
     reuseExistingServer: !process.env.CI,
   },
