63fc758ef61677f1079cd62e22b5e0c861fbf7e0
Joe Hsiao <joe.phsiao@gmail.com>
2025-03-24 00:22:00 -0700

    complete 11.10 - Add code for fly.io deployment

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 /* eslint no-console: "off" */
 
 const express = require('express')
+// const path = require('path')
 const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+// })
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
